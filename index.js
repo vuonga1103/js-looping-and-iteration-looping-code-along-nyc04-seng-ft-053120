60b48de04c28c4a83b594ba9cd82ad3b69f3be16
vuonga1103 <vuonga1103@yahoo.com>
2020-08-02 00:26:56 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// Code your solutions in this file
+function writeCards(names, event) {
+  let result = [];
+  for (let i = 0; i < names.length; i++) {
+    result.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+  }
+  return result;
+}
+
+function countDown(num) {
+  while (num >= 0) {
+    console.log(num);
+    num--;
+  }
+}
